08f0a699e501e6b637ccca0b04b7e5e57c40dc4b
Stephen Compall <scompall@nocandysw.com>
2012-08-11 16:23:05 -0400

    Arithmetic traversal

--- src/main/scala/com/nocandysw/interpmixed/Tree.scala
+++ src/main/scala/com/nocandysw/interpmixed/Tree.scala
@@ -13,13 +13,12 @@ object Languages {
   case class Ref(n: String) extends Environment[Nothing]
 
   object Environment {
-    implicit val environmentInstance: Traverse[Environment] =
-      new Traverse[Environment] {
-	def traverseImpl[G[_]: Applicative,A,B](fa: Environment[A])(f: A => G[B]): G[Environment[B]] = fa match {
-	  case Bind(n, v, b) => f(b) map (Bind(n, v, _))
-	  case Ref(n) => (Ref(n): Environment[B]).point[G]
-	}
+    implicit object environmentInstance extends Traverse[Environment] {
+      def traverseImpl[G[_]: Applicative,A,B](fa: Environment[A])(f: A => G[B]): G[Environment[B]] = fa match {
+	case Bind(n, v, b) => f(b) map (Bind(n, v, _))
+	case Ref(n) => (Ref(n): Environment[B]).point[G]
       }
+    }
   }
 
   /** The language of simple arithmetic. */
@@ -28,6 +27,17 @@ object Languages {
   case class Sub[E](l: E, r: E) extends Arithmetic[E]
   case class Mul[E](l: E, r: E) extends Arithmetic[E]
   case class Div[E](l: E, r: E) extends Arithmetic[E]
+
+  object Arithmetic {
+    implicit object arithmeticInstance extends Traverse[Arithmetic] {
+      def traverseImpl[G[_]: Applicative,A,B](fa: Arithmetic[A])(f: A => G[B]): G[Arithmetic[B]] = fa match {
+	case Add(l, r) => (f(l) <**> f(r))(Add.apply)
+	case Sub(l, r) => (f(l) <**> f(r))(Sub.apply)
+	case Mul(l, r) => (f(l) <**> f(r))(Mul.apply)
+	case Div(l, r) => (f(l) <**> f(r))(Div.apply)
+      }
+    }
+  }
 
   /** The language of primitives. */
   sealed trait Literal[+E]
