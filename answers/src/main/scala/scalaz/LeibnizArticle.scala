9a1db51e4a0bba3775f97d66b7ff0b2e8e427215
Stephen Compall <scompall@nocandysw.com>
2014-06-29 12:59:44 -0400

    Code samples from the Leibniz article.

---
+++ answers/src/main/scala/scalaz/LeibnizArticle.scala
@@ -0,0 +1,22 @@
+package test
+
+final case class XList[A](xs: List[A]) {
+  def sum(implicit ev: A =:= Int): Int =
+    xs.foldLeft(0)(_ + _)
+
+  def sum2(implicit ev: A Leib Int): Int =
+    ev.subst[List](xs).foldLeft(0)(_ + _)
+}
+
+sealed abstract class Leib[A, B] {
+  def subst[F[_]](fa: F[A]): F[B]
+}
+
+object Leib {
+  implicit def refl[A]: Leib[A, A] = new Leib[A, A] {
+    override def subst[F[_]](fa: F[A]): F[A] = fa
+  }
+
+  def lift[F[_], A, B](ab: A Leib B): F[A] Leib F[B] =
+    ab.subst[Lambda[X => (F[A] Leib F[X])]](Leib.refl[F[A]])
+}
