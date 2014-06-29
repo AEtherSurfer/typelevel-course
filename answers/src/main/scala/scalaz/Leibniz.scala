c03f607605bdd6864935f34682efc60ba782e6ab
Stephen Compall <scompall@nocandysw.com>
2014-06-29 12:59:10 -0400

    Leib answers.

--- answers/src/main/scala/scalaz/Leibniz.scala
+++ answers/src/main/scala/scalaz/Leibniz.scala
@@ -3,22 +3,24 @@ package com.nocandysw.typelevelcourse.scalaz
 import scalaz.Id.Id
 
 /** Simpler version of [[scalaz.Leibniz]]. */
-sealed abstract class ===[A, B] extends (A => B) {
+sealed abstract class Leib[A, B] extends (A => B) {
   def subst[F[_]](fa: F[A]): F[B]
 
   // Derived combinators
   final override def apply(a: A): B = subst[Id](a)
 
-  /** === is symmetric. */
-  final def symm: (B === A) = ???
+  /** Leib is symmetric. */
+  final def symm: B Leib A =
+    subst[? Leib A](Leib.refl)
 
-  /** === is transitive. */
-  final def trans[C](bc: (B === C)): (A === C) = ???
+  /** Leib is transitive. */
+  final def trans[C](bc: B Leib C): A Leib C =
+    bc.subst[A Leib ?](this)
 }
 
-object === {
-  /** === is reflexive. */
-  implicit def refl[A]: (A === A) = new (A === A) {
+object Leib {
+  /** Leib is reflexive. */
+  implicit def refl[A]: A Leib A = new (A Leib A) {
     override def subst[F[_]](fa: F[A]) = fa
   }
 }
