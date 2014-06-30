e53427fb596cc5ab589d7723334cd61e278fd416
Stephen Compall <scompall@nocandysw.com>
2014-06-29 23:44:40 -0400

    Rest of Leibniz part 1.

--- answers/src/main/scala/scalaz/LeibnizArticle.scala
+++ answers/src/main/scala/scalaz/LeibnizArticle.scala
@@ -19,4 +19,12 @@ object Leib {
 
   def lift[F[_], A, B](ab: A Leib B): F[A] Leib F[B] =
     ab.subst[Lambda[X => (F[A] Leib F[X])]](Leib.refl[F[A]])
+
+  import scalaz.Leibniz, Leibniz.===
+
+  def toScalaz[A, B](ab: A Leib B): A === B =
+    ab.subst[A === ?](Leibniz.refl)
+
+  def toLeib[A, B](ab: A === B): A Leib B =
+    ab.subst[A Leib ?](Leib.refl)
 }
