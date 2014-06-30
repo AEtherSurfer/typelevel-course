46a231dd1bbfe097644178217443f6f7c19c61d8
Stephen Compall <scompall@nocandysw.com>
2014-06-30 13:58:29 -0400

    Constraint propagation exercises.

--- answers/src/main/scala/shapeless/HList.scala
+++ answers/src/main/scala/shapeless/HList.scala
@@ -1,6 +1,6 @@
 package com.nocandysw.typelevelcourse.shapeless.answers
 
-import shapeless._
+import shapeless._, ops.hlist.Prepend
 
 object HL {
   val lista = 1 :: "Something" :: HNil
@@ -11,4 +11,13 @@ object HL {
   val chosen = listab.select[String]
   // could not find implicit value for parameter selector...
   // val missing = listab.select[List[String]]
+
+  def appendHL[A <: HList, B <: HList](a: A, b: B)
+              (implicit prepend: Prepend[A, B]): prepend.Out =
+    a ++ b
+
+  def appendHL3[A <: HList, B <: HList, C <: HList, I <: HList](a: A, b: B, c: C)
+               (implicit p1: Prepend.Aux[A, B, I],
+                p2: Prepend[I, C]): p2.Out =
+    a ++ b ++ c
 }
--- exercises/src/main/scala/shapeless/HList.scala
+++ exercises/src/main/scala/shapeless/HList.scala
@@ -1,5 +1,7 @@
 package com.nocandysw.typelevelcourse.shapeless
 
+import shapeless._, ops.hlist.Prepend
+
 object HL {
   // Define a couple of hlists
 
@@ -7,4 +9,13 @@ object HL {
 
   // Which value do you get when you ".select[X]" for some X type that
   // appears twice?  What about one that doesn't appear?
+
+  // implement it.  You'll have to add implicit evidence and a result
+  // type
+  def appendHL[A <: HList, B <: HList](a: A, b: B) = ???
+
+  // implement it.  You'll have to add implicit evidence, another type
+  // param, and a result type
+  def appendHL3[A <: HList, B <: HList, C <: HList, I <: HList]
+    (a: A, b: B, c: C) = ???
 }
