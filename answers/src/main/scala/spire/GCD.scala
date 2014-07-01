b3565a36892466ea92340b7177b35d82dc11a17d
Stephen Compall <scompall@nocandysw.com>
2014-07-01 18:48:51 -0400

    scalaz example of syntax, for comparison

--- answers/src/main/scala/spire/GCD.scala
+++ answers/src/main/scala/spire/GCD.scala
@@ -1,5 +1,7 @@
 package typelevelcourse.spire.answers
 
+import scala.{specialized => sp}
+
 import spire.math.Integral
 import spire.implicits._
 
@@ -8,4 +10,10 @@ object GCD {
   def euclid[A: Integral](x: A, y: A): A =
     if (y == 0) x
     else euclid(y, x % y)
+
+  // A similar Scalaz call.
+  def twostrings = {
+    import scalaz.std.string._, scalaz.syntax.monoid._
+    "a" multiply 2
+  }
 }
