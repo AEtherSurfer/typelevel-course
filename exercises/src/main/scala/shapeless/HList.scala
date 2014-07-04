ff3f52808382474e5bbf09ad878ff301be4dda3c
ÆtherSurfer <aethersurfer@aethercraft.com>
2014-07-04 18:11:51 -0400

    playing with hlist

--- exercises/src/main/scala/shapeless/HList.scala
+++ exercises/src/main/scala/shapeless/HList.scala
@@ -5,7 +5,12 @@ import shapeless._, ops.hlist.Prepend
 object HL {
   // Define a couple of hlists
 
+  val h1 = "Hello" :: List('H', 'e', 'l', 'l', 'o') :: "Other" :: HNil
+  val h2 = 42 :: ((a:Int) => {a * a}) :: HNil
+
   // Add them together with ++
+  val h12 = h1 ++ h2
+  val h21 = h2 ++ h1
 
   // Which value do you get when you ".select[X]" for some X type that
   // appears twice?  What about one that doesn't appear?
